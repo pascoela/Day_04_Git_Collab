dfe97787e3c4b9182f020e09a6c257446a51276f
yukailin2 <yukailin@ganymede.egr.msu.edu>
2023-01-24 16:26:45 -0500

    Erie water levels over time

--- Great_Lakes_Water_Levels.py
+++ Great_Lakes_Water_Levels.py
@@ -1,16 +1,4 @@
-""""
-To use this notebook for your in-class assignment, you will need these 
-files, which you shoujld have downloaded:
-* mhu.csv -- Lake Michigan and Lake Huron
-* sup.csv -- Lake Superior
-* eri.csv -- Lake Erie
-* ont.csv -- Lake Ontario
 
-As instructed in the in-class activity notebook for today, you are 
-only expected to complete one PART below. Do not worry if your group 
-is not big enough to finish all parts below, but if you have extra 
-time, you're welcome to do so.
-"""
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
