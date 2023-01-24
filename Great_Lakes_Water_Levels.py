3c31a451c6692b793f083b70a51e250382a23ada
Minh Anh Dang <danganh1@ganymede.egr.msu.edu>
2023-01-24 16:15:24 -0500

    Michigan/Huron Water Levels over time

---
+++ Great_Lakes_Water_Levels.py
@@ -0,0 +1,53 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# PART 1
+# Using the Michigan/Huron Dataset, plot the Water Level, the second 
+# column, as a function of time years
+
+mhu = pd.read_csv("mhu.csv")
+plt.plot(mhu["time"], mhu["lake average"])
+plt.xlabel("Time(Years)")
+plt.ylabel("Water Levels(m)")
+plt.title("Michigan/Huron Water Levels over Time")
+
+# PART 2
+# Using the Superior Dataset, plot the Water Level, the second column, 
+# as a function of time years
+
+
+
+# PART 3
+# Using the Erie Dataset, plot the Water Level, the second column, 
+# as a function of time years
+
+
+
+# PART 4
+# Using the Ontario Dataset, plot the Water Level, the second column, 
+# as a function of time years
+
+
+
+# PART 5
+# Using the Michigan/Huron and Superior Datasets, plot the 
+# Michigan/Hurion Water Level vs Superior Water Level to see if there 
+# is any correlation between the water levels.
+
+
+
+# PART 6
+# Using the Michigan/Hurion and Erie Datasets, plot the 
+# Michigan/Huron Water Level vs Erie Water Level to see if there is 
+# any correlation between the water levels.
+
+
+
+# PART 7
+# Using the Superior and Ontario Datasets, plot the Superior Water 
+# Level vs Ontario Water Level to see if there is any correlation 
+# between the water levels.
+
+
